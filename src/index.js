91df4b17a93d270efe367ad48a98682432559ba5
최재선 <js@choejaeseon-ui-MacBookPro.local>
2021-07-25 19:42:45 +0900

    클라 완성

--- src/index.js
+++ src/index.js
@@ -7,39 +7,72 @@ const socket = socketIO('http://localhost:4000');
 
 class App extends React.Component {
   state = {
-    output: 0
+    output: 0,
+    curNum: 0,
   }
 
-  callApi = () =>{
-    console.log('iocall');
+  setOutput = (value) => {
+    this.setState(() => {
+      return {output: value};
+    })
+  }
+
+  setCurNum = (value) => {
+    this.setState(() => {
+      return {curNum: value};
+    })
+  }
+
+  callStart = () =>{
+    console.log('startCall');
 
     socket.on("connect", () => {
-      console.log('wow');
+      console.log('connected server');
+    });
+
+    socket.emit("start", { output: this.state.output, curNum: this.state.curNum }); 
+    socket.on("start", req => {
+      console.log(req, this.state.output);
+      this.setOutput(req.output);
+      this.setCurNum(req.curNum);
+    });
+  }
+
+  callPause = () => {
+    socket.emit("pause");
+  }
+
+  callCancel = () => {
+    socket.emit("cancel");
+    socket.on("cancel", req => {
+      this.setOutput(req.output);
+      this.setCurNum(req.curNum);
     })
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.callApi}>
-          start
+        <button onClick={this.callStart}>
+          start / resume
         </button>
-        <button onClick={function() { alert('click'); }}>
+        <button onClick={this.callPause}>
           pause
         </button>
-        <button onClick={function() { alert('click'); }}>
+        <button onClick={this.callCancel}>
           cancel
         </button>
 
         <div>
-          {this.state.output}
+          합계 : {this.state.output}
+        </div>
+        <div>
+          진행중인 숫자 : {this.state.curNum}
         </div>
       </div>
     )
   }
 }
-
-// ========================================
 
 ReactDOM.render(
   <App />,
